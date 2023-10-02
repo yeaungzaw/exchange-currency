0cadcdfc05da697884e607ba585a9c20863086e3
yeaungzaw <yeaungzaw.learner@gmail.com>
2023-10-03 02:22:26 +0630

    update

--- js/app.js
+++ js/app.js
@@ -13,17 +13,17 @@ const calc = (str1, str2) => {
 
     if (input.value !== '') {
         if ((str1 === 'usd') && (str2 === 'mmk')) {
-            output.value = input.valueAsNumber * 3000;
+            output.value = (input.valueAsNumber * 3000);
         } else if ((str1 === 'usd' && (str2 === 'thb'))) {
             output.value = (input.valueAsNumber * 3000) / 100;
         } else if ((str1 === 'mmk') && (str2 === 'usd')) {
-            output.value = input.valueAsNumber / 3000;
+            output.value = (input.valueAsNumber / 3000).toFixed(2);
         } else if ((str1 === 'mmk') && (str2 === 'thb')) {
-            output.value = input.valueAsNumber / 100;
+            output.value = (input.valueAsNumber / 100).toFixed(2);
         } else if ((str1 === 'thb') && (str2 === 'mmk')) {
             output.value = input.valueAsNumber * 100;
         } else if ((str1 === 'thb') && (str2 === 'usd')) {
-            output.value = (input.valueAsNumber * 100) / 3000;
+            output.value = ((input.valueAsNumber * 100) / 3000).toFixed(2);
         }
     }
 
@@ -38,11 +38,13 @@ downAndUpBtn.onclick = () => {
     let t = to.innerHTML;
     let i = input.value;
     let o = output.value;
-
-
+    let fc = fromCountry.value;
+    let tc = toCountry.value;
 
     from.innerHTML = t;
     to.innerHTML = f;
     input.value = o;
     output.value = i;
+    fromCountry.value = tc;
+    toCountry.value = fc;
 }
